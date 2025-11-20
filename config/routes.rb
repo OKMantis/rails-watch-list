4af7af2c5f5a2949cfaa8359e84228d308cc5a80
Owen Moss <33687855+OKMantis@users.noreply.github.com>
2025-11-20 22:18:47 +0100

    created the bookmarks controller, unfinished

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :movies
-  resources :lists
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
