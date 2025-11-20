738c6a807589516eeb13527d0c8f29fda763b065
Owen Moss <33687855+OKMantis@users.noreply.github.com>
2025-11-20 16:46:52 +0100

    added show list feature

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,20 @@
 class ListsController < ApplicationController
-
+  before_action :set_list, only: [:show]
   def index
     @lists = List.all
   end
 
+  def show
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 
 end
