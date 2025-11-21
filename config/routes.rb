54bdd01b25e4b652d60dc984077840bfdd729a7d
Owen Moss <33687855+OKMantis@users.noreply.github.com>
2025-11-21 15:15:52 +0100

    fixed bug routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root to: "lists#index"
+  
   resources :movies
-  resources :lists do
+  resources :lists, except: [:index] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
